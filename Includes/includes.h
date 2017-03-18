c295e85bbdab082086e0383ccee523bd54d42db5
Gabriel Araújo <gabriel_feg@hotmail.com>
2017-03-18 18:14:04 -0300

    Bibliotecas usadas no programa

---
+++ Includes/includes.h
@@ -0,0 +1,17 @@
+/**
+ * Todas as bibliotecas usadas no prograna
+ */
+
+#ifndef __INCLUDES_H_
+#define __INCLUDES_H_
+
+
+	#include <iostream> //std::cin, std::cout, std::endl
+	#include <vector> //std::vector
+	#include <algorithm> //std::find, std::binary_search
+	#include <iterator> //begin(), size(), end()
+	#include <sstream> //std::stringstream
+	#include <chrono> // std::chrono::duration<double, std::milli>, std::chrono::steady_clock::now()
+	#include <fstream> //std::ofstream
+
+#endif                
